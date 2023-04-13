af9cb94fbdc16192e7107269ae86c2f710266b3d
olfarejeb <rejebolfa71@gmail.com>
2023-04-13 23:30:07 +0100

    delete file not needed

--- src/test/java/com/gestion/filmotheque/repository/ActeurRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gestion.filmotheque.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gestion.filmotheque.entities.Acteur;
-
-public interface ActeurRepository extends JpaRepository <Acteur ,Integer>{
-
-}
--- src/test/java/com/gestion/filmotheque/repository/CategorieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gestion.filmotheque.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gestion.filmotheque.entities.Categorie;
-
-public interface CategorieRepository extends JpaRepository <Categorie ,Integer>{
-
-}
